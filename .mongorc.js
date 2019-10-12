d7ae125e8585ab0416b3774b3df28990c1be6465
Abhishek Jaisingh <abhi2254015@gmail.com>
2019-10-13 02:25:16 +0530

    [.mongorc] Replace Symlink With Actual File
    * Add link to .mongorc in README

--- .mongorc.js
+++ .mongorc.js
@@ -1,1 +1,24 @@
-/home/abhishek/.mongorc.js
+// .mongorc.js
+
+// display db name in prompt
+prompt = function() {
+    if (typeof db == 'undefined') {
+        return '(nodb)> ';
+    }
+
+    // Check the last db operation
+    try {
+        db.runCommand({getLastError:1});
+    }
+    catch (e) {
+        print(e);
+    }
+
+    return db+"> ";
+};
+
+
+var compliment = ["attractive", "intelligent", "like Batman"];
+var index = Math.floor(Math.random()*3);
+
+print("Hello, you're looking particularly "+compliment[index]+" today!");
